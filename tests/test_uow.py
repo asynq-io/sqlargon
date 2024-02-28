ddf4dc71089e83857dc27b1e8891701f97161fe7
RaRhAeu <radzim_ko@wp.pl>
2024-02-28 09:48:52 +0100

    fix: uow context manager

--- tests/test_uow.py
+++ tests/test_uow.py
@@ -8,3 +8,13 @@ async def test_uow(db, user_repository_class):
     uow = TestUow(db)
     assert isinstance(uow.users, user_repository_class)
     assert uow.users.db is db
+
+
+async def test_uow_context_manager(db, user_repository_class):
+    class TestUow(SQLAlchemyUnitOfWork):
+        users: user_repository_class
+
+    uow = TestUow(db)
+    async with uow:
+        assert isinstance(uow.users, user_repository_class)
+        await uow.users.all()
